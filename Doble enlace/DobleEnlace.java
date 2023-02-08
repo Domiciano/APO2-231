35f113cba81c4a454a46c6d64a4f08650d2725e4
Domiciano Rincon Niño <1143848922@icesi.edu.co>
2023-02-08 10:12:00 -0500

    Semana 2

---
+++ Doble enlace/DobleEnlace.java
@@ -0,0 +1,64 @@
+public class DobleEnlace {
+    private Node head;
+    private Node tail;
+
+    public void addNode(Node node){
+        if(head==null){
+            head = node;
+            tail = node;
+        }else{
+            tail.setNext(node);
+            node.setPrevious(tail);
+            tail = node;
+        }
+    }
+
+    public void addFirst(Node node){
+        if(head==null){
+            head = node;
+            tail = node;
+        }else{
+            node.setNext(head);
+            head.setPrevious(node);
+            head = node;
+        }
+    }
+
+    //Activacion
+    public void print(){
+        print(head);
+    }
+
+    //Recursivo
+    private void print(Node current){
+        if(current == null){
+            return;
+        }
+        System.out.println(current.getName());
+        print(current.getNext());
+    }
+
+    //Eliminacion
+    public void delete(String goal){
+        delete(head, goal);
+    }
+    private void delete(Node current, String goal){
+        if(current == null){
+            return;
+        }
+        if(current.getName().equals(goal)){
+            //Eliminar
+            Node prev = current.getPrevious();
+            Node next = current.getNext();
+            prev.setNext(next);
+            next.setPrevious(prev);
+            return;
+        }
+        delete(current.getNext(), goal);
+
+    }
+
+
+
+
+}
