f762ad60f8b6ed59b73ddfb79befa1da26d42b99
Domiciano Rincon Niño <1143848922@icesi.edu.co>
2023-02-22 10:00:22 -0500

    Semana 4

--- Arboles/Main.java
+++ Arboles/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         BST tree = new BST();
-        Node a = new Node("Miguel");
-        Node b = new Node("Sebastian");
-        Node c = new Node("Juan");
-        Node d = new Node("Santiago");
-        Node e = new Node("Lina");
-        Node f = new Node("Laura");
+        Node a = new Node("David");
+        Node b = new Node("Julian");
+        Node c = new Node("Pablo");
+        Node d = new Node("Juan");
+        Node e = new Node("Isabela");
+        Node f = new Node("Daniela");
         tree.add(a);
         tree.add(b);
         tree.add(c);
@@ -15,7 +15,14 @@ public class Main {
         tree.add(e);
         tree.add(f);
 
-        System.out.println(e.getLeft().getKey());
+        Node n = tree.getMax();
+        System.out.println(n.getKey());
+
+        tree.inOrder();
+
+        Node search = tree.search("Julian");
+        System.out.println(search.getKey());
+        System.out.println(search.getRight().getKey());
 
 
     }
